fa461b48f4d8dc6f884268d5a789a1e9db1e20fe
ibrahim-delice <100766765+ibrahim-delice@users.noreply.github.com>
2022-12-15 11:33:48 +0200

    part 6: dynamic parameter

--- packages/addons/turbo-posts-api-mocks/src/posts/handlers.ts
+++ packages/addons/turbo-posts-api-mocks/src/posts/handlers.ts
@@ -5,4 +5,14 @@ export const turboBlogApiMockedPostsHandlers = [
   rest.get('https://dummyjson.com/posts', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ ...mockedPosts }))
   }),
+  rest.get('https://dummyjson.com/posts/:id', (req, res, ctx) => {
+    const { postId } = req.params
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        ...mockedPosts.posts.find((post) => post.id === Number(postId)),
+      }),
+    )
+  }),
 ]
